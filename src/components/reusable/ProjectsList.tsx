764848f6faa682a83a5f723ba8408940f9a61753
urluconeptual <ciocan_alexandra16@yahoo.com>
2024-01-02 16:59:18 +0200

    added search functionality

--- src/components/reusable/ProjectsList.tsx
+++ src/components/reusable/ProjectsList.tsx
@@ -3,33 +3,56 @@ import { observer } from "mobx-react";
 import { ProjectData, projectStore } from "../../stores/projectsStore";
 import { useEffect, useState } from "react";
 import ProjectCard from "./ProjectCard";
+import Search from "antd/es/input/Search";
 
 const ProjectsList = observer(() => {
   const [dataSource, setDataSource] = useState<ProjectData[]>([]);
+  const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
     setDataSource(projectStore.currentViewProjects);
   }, [projectStore.currentViewProjects]);
 
+  const filteredDataSource =
+    searchValue === ""
+      ? dataSource
+      : dataSource.filter(
+          (item) =>
+            item.title.includes(searchValue) ||
+            item.authorEmail.includes(searchValue) ||
+            item.githubLink.includes(searchValue) ||
+            item.description.includes(searchValue)
+        );
+
   return (
-    <List
-      itemLayout="vertical"
-      size="large"
-      dataSource={dataSource}
-      pagination={
-        dataSource.length > 1
-          ? {
-              pageSize: 3,
-            }
-          : false
-      }
-      renderItem={(item: ProjectData) => (
-        <List.Item key={item.id} style={{ paddingLeft: 0, paddingRight: 0 }}>
-          <ProjectCard project={item} />
-        </List.Item>
+    <div>
+      {dataSource.length > 1 && (
+        <Search
+          placeholder="Search..."
+          onSearch={(input) => setSearchValue(input)}
+          style={{ width: 300, paddingTop: 20, paddingBottom: 20 }}
+        />
       )}
-      style={{ width: 1200 }}
-    />
+      <div>{filteredDataSource.length} results</div>
+      <List
+        itemLayout="vertical"
+        size="large"
+        dataSource={filteredDataSource}
+        pagination={
+          dataSource.length > 1
+            ? {
+                pageSize: 4,
+              }
+            : false
+        }
+        renderItem={(item: ProjectData) => (
+          <List.Item key={item.id} style={{ paddingLeft: 0, paddingRight: 0 }}>
+            <ProjectCard project={item} />
+          </List.Item>
+        )}
+        style={{ width: 1200 }}
+      />
+    </div>
   );
 });
 
